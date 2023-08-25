bb5cee90967151d35308356fca698e670c0c254a
Sugam Kumar <103991801+gitskr26hub@users.noreply.github.com>
2023-08-25 11:10:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const cors = require("cors");
 ////////////////////////////////////////////////////////////////
 var fsWin = require("fswin");
+const e = require("express");
 
 app.use(express.json());
 app.use(cors());
@@ -26,23 +27,18 @@ var attributes = {
   IS_PINNED: false,
 };
 
+//set Data in the folder and the file
 app.post("/post", async (req, res) => {
-  const folderName = "C:/UserPrivatedata";
-  console.log("request body",req.body)
-  res.send("req body")
-    try {
+  const folderName = "C:/UserPrivetdata";
+
+  try {
     if (!fs.existsSync(folderName)) {
       fs.mkdirSync(folderName);
 
-      fs.writeFileSync(
-        folderName + "/userData.json",
-        JSON.stringify(req.body),
-        function (err) {
-          if (err) throw err;
-          res.status(200).json({ msg: "Success" });
-          console.log("File is created successfully.");
-        }
-      );
+      fs.writeFileSync(folderName + "/userData.json", JSON.stringify(req.body));
+
+      res.status(200).json({ msg: "Success" });
+      console.log("File is created successfully.");
 
       fsWin.setAttributes(
         folderName,
@@ -57,20 +53,22 @@ app.post("/post", async (req, res) => {
         }
       );
     } else {
-      fs.writeFileSync(
+       fs.unlink( folderName + "/userData.json",function(err){
+        if (err) throw err;
+      })
+
+      fs.writeFile(
         folderName + "/userData.json",
         JSON.stringify(req.body),
         function (err) {
           if (err) throw err;
-          res.status(200).json({ msg: "Success" });
-          console.log("File is created successfully.");
         }
       );
-
+      res.status(200).json({ msg: "Successfully updated" });
+      console.log("File is updated successfully.");
       fsWin.setAttributes(
-        folderName+"/userData.json",
+        folderName + "/userData.json",
         { IS_HIDDEN: true },
-
         (fswinErr) => {
           if (fswinErr) {
             console.error("Error setting hidden attribute:", fswinErr);
@@ -85,24 +83,29 @@ app.post("/post", async (req, res) => {
   }
 });
 
+// get data from the file backend
 app.get("/get", (req, res) => {
-  const folderName = "C:/UserPrivatedata";
-  if (!fs.existsSync(folderName))
-    res
-      .status(500)
-      .send({ message: "NO data Found,please create data first!" });
+  const folderName = "C:/UserPrivetdata";
+ 
+try{
+  if (!fs.existsSync(folderName)){
+  res
+    .status(500)
+    .send({ message: "NO data Found,please create data first!" });
+}
+if (fs.existsSync(folderName + "/userData.json")) {
+  const filePath = "C:/UserPrivetdata/userData.json";
+  const data = fs.readFileSync(filePath, "utf-8");
 
-  if (fs.existsSync(folderName + "/userData.json")) {
-    const filePath = "C:/UserPrivatedata/userData.json";
-    const data = fs.readFileSync(filePath, "utf-8");
+  res.status(200).json({ msg: "Success", data: JSON.parse(data) });
+  console.log("send successfully.");
+  //  fsWin.setAttributes(folderName + "/userData.json", { IS_HIDDEN: true });
+} else res.status(500).send({ message: "NO data Found,please create data first!" });
+}
+catch(err){
+  console.log(err)
+}
 
-    res.status(200).json({ msg: "Success", data: JSON.parse(data) });
-    console.log("send successfully.");
-    fsWin.setAttributes(folderName + "/userData.json", { IS_HIDDEN: true });
-  } else
-    res
-      .status(500)
-      .send({ message: "NO data Found,please create data first!" });
 });
 
 app.listen(8080, () => {
